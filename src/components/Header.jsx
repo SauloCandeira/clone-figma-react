1e3459fee64cc0ad1a1281f445a9e0635b8d4f76
Saulo Candeira <saulocandeira@hotmail.com>
2023-09-22 13:01:22 -0300

    Add: .

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Header() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className="read-the-docs">
+        Header
+      </p>
+    </>
+  )
+}
+
+export default Header
