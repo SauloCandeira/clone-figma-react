ed3aaf0ad345c88828b391aaf79e23a49aabca49
Saulo Candeira <saulocandeira@hotmail.com>
2023-09-22 14:42:27 -0300

    ADD

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,39 @@
-import ClarifionLogo from '../../public/clarifion-logo.png'
-import McafeeLogo from '../../public/Mcafee.svg'
-import NortonLogo from '../../public/Norton.svg'
+import React from 'react';
+import ClarifionLogo from '../../public/clarifion-logo.png';
+import McafeeLogo from '../../public/Mcafee.svg';
+import NortonLogo from '../../public/Norton.svg';
 
 function Header() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className="width: 1500px; padding-left: 127px; padding-right: 127px; padding-top: 30px; padding-bottom: 30px; left: 0px; top: 50px; position: absolute; background: white; justify-content: space-between; align-items: center; display: inline-flex">
+    <div
+      style={{
+        width: '1500px',
+        paddingLeft: '127px',
+        paddingRight: '127px',
+        paddingTop: '30px',
+        paddingBottom: '30px',
+        left: '0px',
+        top: '50px',
+        position: 'absolute',
+        background: 'white',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        display: 'inline-flex',
+      }}
+    >
+      <img style={{ width: '192px', height: '36px' }} src={ClarifionLogo} alt="Clarifion logo" />
 
-          <img className="width: 192px; height: 36px" src={ClarifionLogo} />
+      <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: '32px', display: 'flex' }}>
+        <div style={{ width: '88px', height: '32px', position: 'relative' }}>
+          <img src={McafeeLogo} style={{ width: '88px', height: '32px', left: '0px', top: '0px', position: 'absolute' }} alt="Mcafee logo" />
+        </div>
 
-          <div className="justify-content: flex-start; align-items: flex-start; gap: 32px; display: flex">
-
-              <div className="width: 88px; height: 32px; position: relative">
-                  {/* <img style="width: 88px; height: 32px; left: 0px; top: 0px; position: absolute" src=''/> */}
-                  <img src={McafeeLogo} className="width: 88px; height: 32px; left: 0px; top: 0px; position: absolute" alt="Mcafee logo" />
-              </div>
-              
-              <div className="width: 82px; height: 32px; position: relative">
-                  <img src={NortonLogo} className="width: 88px; height: 32px; left: 0px; top: 0px; position: absolute" alt="Norton logo" />
-                  {/* <img style="width: 88px; height: 32px; left: 0px; top: 0px; position: absolute" src='./../assets/images/Norton.svg' /> */}
-              </div>
-          </div>
+        <div style={{ width: '82px', height: '32px', position: 'relative' }}>
+          <img src={NortonLogo} style={{ width: '88px', height: '32px', left: '0px', top: '0px', position: 'absolute' }} alt="Norton logo" />
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
